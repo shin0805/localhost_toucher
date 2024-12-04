dc0d8d5ea37d802c9946e57bcb46c1b5f65b1b5e
Shintaro Inoue <shintaro-inoue-mac@g.ecc.u-tokyo.ac.jp>
2024-12-04 23:46:06 +0900

    fix: save to list

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
+import pigpio
 import time
+
+SERVO_PIN = 18
+pi = pigpio.pi()
 
 
 def fetch_localhost_content():
@@ -16,17 +20,27 @@ def fetch_localhost_content():
 def extract_numbers(input_string):
   # Split the string by colon (":") and comma (",") to isolate the numbers
   parts = input_string.split(":")[-1].split(",")
-
   # Strip whitespace and convert the parts to integers
   numbers = [int(part.strip()) for part in parts if part.strip().isdigit()]
-
   # Take the last 5 numbers and return them
   return numbers[-5:]
+
+
+def set_angle(angle):
+  assert 46 <= angle <= 130, '角度は46から130の間でなければなりません'
+  pulse_width = (angle / 180) * (2500 - 500) + 500
+  pi.set_servo_pulsewidth(SERVO_PIN, pulse_widh)
+
+
+def map_to_range(value, src_min, src_max, dst_min, dst_max):
+  return dst_min + (value - src_min) * (dst_max - dst_min) / (src_max - src_min)
 
 
 if __name__ == "__main__":
   while True:
     text = fetch_localhost_content()
     nums = extract_numbers(text)
-    print(nums)
+    angles = [map_to_range(num, 0, 1000, 46, 130) for num in nums]
+    # print(nums)
+    print(angles)
     time.sleep(0.05)
