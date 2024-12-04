b3dce4570c7722ce5448d2b0262961671578b25c
Shintaro Inoue <shintaro-inoue-mac@g.ecc.u-tokyo.ac.jp>
2024-12-04 23:21:08 +0900

    add: main.py and test localhost

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def fetch_localhost_content():
+  url = "http://192.168.10.105:8081"
+  try:
+    response = requests.get(url)
+    response.raise_for_status()  # HTTPエラーがあれば例外を発生
+    print("HTTP Status Code:", response.status_code)
+    print("Content:")
+    print(response.text)
+  except requests.exceptions.RequestException as e:
+    print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+  fetch_localhost_content()
