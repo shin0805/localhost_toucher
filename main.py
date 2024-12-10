2fdaa9f718ee4b37c022535934ffcb88fbf1d922
Shintaro Inoue <shintaro-inoue-mac@g.ecc.u-tokyo.ac.jp>
2024-12-11 00:26:03 +0900

    add: switch

--- main.py
+++ main.py
@@ -52,11 +52,18 @@ def map_to_range(value, src_min, src_max, dst_min, dst_max):
 if __name__ == "__main__":
   start_time = time.time()
   pre_switch = False
+  # パラメータ
+  SWITCH_OFF_ANGLE = 0  # スイッチオフの角度 (deg)
+  SWITCH_ON_ANGLE = 30  # スイッチオンの角度 (deg)
+  SWITCH_CYCLE_TIME = 2  # スイッチの動作周期時間 (sec) (整数)
+  SWITCH_ON_TIME = 0.5  # スイッチオンのための時間 (sec) (動作周期時間よりは短く)
+
+  set_angle(10, SWITCH_OFF_ANGLE)
   while True:
     now_time = time.time() - start_time
     text = fetch_localhost_content()
     nums = extract_numbers(text)
-    angles = [0, 0, 0, 0, 0]
+    angles = [0, 0, 0, 0, 0, 0]
     angles[0] = map_to_range(nums[0], 0, 6000, 46, 130)  # touch designerのMIN, MAX, サーボのMIN, MAX
     angles[1] = map_to_range(nums[1], 0, 6000, 46, 130)
     angles[2] = map_to_range(nums[2], 0, 6000, 46, 130)
@@ -70,11 +77,11 @@ if __name__ == "__main__":
     elif nums[5] == 0:
       pre_switch = False
     if pre_switch:
-      if (now_time - swich_time) % 2 <= 0.5:
-        set_angle(10, 30)  # スイッチオン
-        angles = angles + [30]
+      if (now_time - swich_time) % int(SWITCH_CYCLE_TIME) <= SWITCH_ON_TIME:
+        set_angle(10, SWITCH_ON_ANGLE)
+        angles[5] = SWITCH_ON_ANGLE
       else:
-        set_angle(10, 0)  # スイッチオフ
-        angles = angles + [0]
+        set_angle(10, SWITCH_OFF_ANGLE)
+        angles[5] = SWITCH_OFF_ANGLE
     print(f"angles: {angles}, touchdesigner: {nums}")
     time.sleep(0.05)
