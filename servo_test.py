a1a4c6b6857a72d98d5f8abb74bd03fde2fabcf0
Shintaro Inoue <shintaro-inoue-mac@g.ecc.u-tokyo.ac.jp>
2024-12-10 23:46:18 +0900

    add: big servo

--- servo_test.py
+++ servo_test.py
@@ -6,12 +6,18 @@ pwm = Adafruit_PCA9685.PCA9685(busnum=1)
 pwm.set_pwm_freq(60)
 
 
-def set_angle(index, angle):
+def set_angle(index, angle, for_switch=False):
   global pwm
-  pulse_min = 123 - 15
-  pulse_max = 590 - 15
-  pulse = int((angle / 180) * (pulse_max - pulse_min) + pulse_min)
-  pwm.set_pwm(index, 0, pulse)
+  if not for_switch:
+    pulse_min = 123 - 15
+    pulse_max = 590 - 15
+    pulse = int((angle / 180) * (pulse_max - pulse_min) + pulse_min)
+    pwm.set_pwm(index, 0, pulse)
+  else:
+    pulse_min = 123
+    pulse_max = 614
+    pulse = int((angle / 180) * (pulse_max - pulse_min) + pulse_min)
+    pwm.set_pwm(index, 0, pulse)
 
 
 def main():
@@ -19,19 +25,21 @@ def main():
   while True:
     angle = 0
     print(f'{angle}')
-    set_angle(0, angle)
-    set_angle(2, angle)
-    set_angle(4, angle)
-    set_angle(6, angle)
-    set_angle(8, angle)
+    # set_angle(0, angle)
+    # set_angle(2, angle)
+    # set_angle(4, angle)
+    # set_angle(6, angle)
+    # set_angle(8, angle)
+    set_angle(10, angle)
     time.sleep(1)
     angle = 180
     print(f'{angle}')
-    set_angle(0, angle)
-    set_angle(2, angle)
-    set_angle(4, angle)
-    set_angle(6, angle)
-    set_angle(8, angle)
+    # set_angle(0, angle)
+    # set_angle(2, angle)
+    # set_angle(4, angle)
+    # set_angle(6, angle)
+    # set_angle(8, angle)
+    set_angle(10, angle)
     time.sleep(1)
 
 
